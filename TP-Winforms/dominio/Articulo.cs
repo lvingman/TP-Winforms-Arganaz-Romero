88df2b044184753ee4a4309d318ffb98d6343321
lvingman <45737515+lvingman@users.noreply.github.com>
2022-09-05 19:40:50 -0300

    Agrega clases de dominio y corrige errores del projecto

---
+++ TP-Winforms/dominio/Articulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Winforms.dominio
+{
+    class Articulo
+    {
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        //Revisar Marca y Categoria
+
+        public string URLImagen { get; set; }
+
+        public float Precio { get; set; }
+
+    }
+}
