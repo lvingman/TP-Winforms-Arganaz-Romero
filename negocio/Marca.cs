10f3a99a53fa986adf5234acf3ae314409846c5d
Gaston097 <gaston.arganaraz97@hotmail.com>
2022-09-11 09:38:58 -0300

    Marca
    Se agrega MarcaNegocio para un listado

---
+++ negocio/Marca.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion from MARCAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.ID = (int)datos.Lector["Id"];
+                    aux.Descripcion = (String)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
